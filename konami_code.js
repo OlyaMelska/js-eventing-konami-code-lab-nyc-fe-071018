0b3b4f46acce9d25f8a5f88014a9c692bd84ab6c
Olha Melska <olya.melska@gmail.com>
2018-08-22 00:44:28 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 const body = document.querySelector('body');
 let tracker = 0;
 
-
-function init() {
+function onKeyDownHandler(event) {
   // Write your JavaScript code inside the init() function
   body.addEventListener('keydown', (event) => {
     if(tracker === code.length){
@@ -25,4 +24,4 @@ function init() {
   })
 }
 
-init()
+onKeyDownHandler()
